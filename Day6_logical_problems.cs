f11a18f6c4362c65d1a356b559058c2b8d046424
parthgarg977 <67135234+Parthgarg977@users.noreply.github.com>
2022-03-12 08:05:28 +0530

    Add files via upload

--- Day6_logical_problems.cs
+++ Day6_logical_problems.cs
@@ -80,6 +80,19 @@ public class FibonacciExample
                 Console.Write("Reversed Number: " + reverse);
                 break;
             case 5:
+                char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
+                int max = 100000000;
+                int random = (int)(Math.random() * max);
+                StringBuffer sb = new StringBuffer();
+
+                while (random > 0)
+                {
+                    sb.append(chars[random % chars.length]);
+                    random /= chars.length;
+                }
+
+                String couponCode = sb.toString();
+                System.out.println("Coupon Code: " + couponCode);
                 break;
             case 6:
                 var watch = Stopwatch.StartNew();
